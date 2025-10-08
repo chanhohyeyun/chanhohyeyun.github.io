7c15b789fc946ef464b21529d87b3f2eef1a6a30
chanhohyeyun <binstars2@naver.com>
2025-10-09 01:08:27 +0900

    Update card.show.opening.js

--- css/wd/card.show.opening.js
+++ css/wd/card.show.opening.js
@@ -168,7 +168,7 @@ function splash_v2(isBgm, isGarland) {
         renderer: 'svg',
         loop: false,
         autoplay: false,
-        path: "/css/wd/name_v1.json"
+        path: "/css/wd/final_version.json"
     });
 
     // DOMLoaded 이벤트 리스너를 별도로 정의
